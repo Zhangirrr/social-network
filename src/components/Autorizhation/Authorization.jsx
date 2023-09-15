4ab4abf691fe5589fa8402bb54a71db3f05780b0
Zhangirrr <zangiruakasov@gmail.com>
2023-09-15 23:47:55 +0600

    final

--- src/components/Autorizhation/Authorization.jsx
+++ src/components/Autorizhation/Authorization.jsx
@@ -8,20 +8,17 @@ import Input from "../Input/Input";
 import Button from "../Button/Button";
 import { useNavigate } from "react-router-dom";
 
-
 function Authorization() {
+  const navigate = useNavigate();
 
-  const navigate = useNavigate()
-
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const { enqueueSnackbar } = useSnackbar();
 
-  const { error, isLoading } = useSelector(store => store.user)
-  
+  const { error, isLoading } = useSelector((store) => store.user);
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [users, setUsers] = useState({});
-
 
   const onEmailChange = (event) => {
     setEmail(event.target.value);
@@ -31,49 +28,51 @@ function Authorization() {
   };
 
   useEffect(() => {
-    if(error){
-      enqueueSnackbar({ message: error, variant: "error" })
-      dispatch(dismissError())
+    if (error) {
+      enqueueSnackbar({ message: error, variant: "error" });
+      dispatch(dismissError());
     }
-  }, [error])
-  
+  }, [error]);
 
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    if(!email){
-      enqueueSnackbar({message: "There is no E-mail", variant: "error"});
+    if (!email) {
+      enqueueSnackbar({ message: "There is no E-mail", variant: "error" });
     }
-    if(!password){
-      enqueueSnackbar({message: "There is no password", variant: "error"});
+    if (!password) {
+      enqueueSnackbar({ message: "There is no password", variant: "error" });
     }
 
-    dispatch(authUser({login: email, password})).then(() => {
-      navigate("/")
-      enqueueSnackbar({ message: "Great job, You've authorized", variant: "success" });
+    dispatch(authUser({ login: email, password })).then(() => {
+      navigate("/");
+      enqueueSnackbar({
+        message: "Great job, You've authorized",
+        variant: "success",
+      });
     });
-
   };
 
   return (
-    
-      <form className={s.email_address} onSubmit={onSubmit}>
-        <div className={s.inputs}>
-          <Input
-            value={email}
-            onChange={onEmailChange}
-            placeholder="Your email"
-            label="Email address"
-          />
-          <Input
-            value={password}
-            onChange={onPasswordChange}
-            placeholder="Password"
-            label="Password"
-          />
-        </div>
-        <Button type="submit" disabled={isLoading}>{isLoading ? "Loading" : "Sign In"}</Button>
-      </form>
+    <form className={s.email_address} onSubmit={onSubmit}>
+      <div className={s.inputs}>
+        <Input
+          value={email}
+          onChange={onEmailChange}
+          placeholder="Your email"
+          label="Email address"
+        />
+        <Input
+          value={password}
+          onChange={onPasswordChange}
+          placeholder="Password"
+          label="Password"
+        />
+      </div>
+      <Button type="submit" disabled={isLoading}>
+        {isLoading ? "Loading" : "Sign In"}
+      </Button>
+    </form>
   );
 }
 
